2621056458166b789a2521844e7b59d725b77b51
Mark Ra <mark.ra.vilbiks@gmail.com>
2024-11-19 06:10:34 +0200

    takes an update rate argument

--- Main.java
+++ Main.java
@@ -11,22 +11,36 @@ import javax.swing.JTextField;
 class Program {
     private static final int height = 300; 
     private static final int width = 500; 
-    private static final int dt = 1000/600; 
+    private static int dt = 1000000/200; 
     private static BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
     private static Entity entity;
     private static JFrame frame = new JFrame("my cool window!");
 
     public static void main(String[] args) {
+        //argumetn parsing
+        if (args.length == 1) {
+            try {
+                dt = 1000000/Integer.valueOf(args[0]);
+            } catch (Exception e) {
+                System.out.println("argument is not an integer, defaulting to " + 1000000/dt + " updates per sec");
+            }
+        } else {
+            System.out.println("update rate argument not provided, defaulting to " + 1000000/dt + " updates per sec");
+        }
+
+        //make black image
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                image.setRGB(x, y, convertARGB(0));  // Set pixel color
+                image.setRGB(x, y, convertARGB(0));
             }
         }
         entity = new Entity(width/2, height/2);
 
+        //image update loop
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        executor.scheduleAtFixedRate(Program::run, 0, dt, TimeUnit.MILLISECONDS);
+        executor.scheduleAtFixedRate(Program::run, 0, dt, TimeUnit.MICROSECONDS);
 
+        //make the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new JTextField("first window!"));// equivalent to: `frame.add(new JTextField("first window!"));`
         frame.add(new JLabel(new ImageIcon(image)));
@@ -34,29 +48,26 @@ class Program {
         frame.setVisible(true);
     }
 
+    //update loop
     public static void run() {
         image.setRGB(entity.x, entity.y, convertARGB(255,170,170,170));
         int rnd = (int) Math.floor(Math.random()*4);
         switch (rnd) {
             case 0:
-                System.out.println("entity at - x: " + entity.x + " y: " + entity.y);
                 entity.x += 1;
-                if (entity.x > width) entity.x = 0;
+                if (entity.x > width) entity.x = 1;
                 break;
             case 1:
-                System.out.println("entity at - x: " + entity.x + " y: " + entity.y);
                 entity.x -= 1;
-                if (entity.x < width) entity.x = width;
+                if (entity.x < 0) entity.x = width-1;
                 break;
             case 2:
-                System.out.println("entity at - x: " + entity.x + " y: " + entity.y);
                 entity.y += 1;
-                if (entity.y > width) entity.y = 0;
+                if (entity.y > height) entity.y = 1;
                 break;
             case 3:
-                System.out.println("entity at - x: " + entity.x + " y: " + entity.y);
                 entity.y -= 1;
-                if (entity.y < width) entity.y = height;
+                if (entity.y < 0) entity.y = height-1;
                 break;
             default:
                 break;
@@ -66,6 +77,7 @@ class Program {
     }
 
 
+    //utility funcs
     private static int convertARGB(int a, int r, int g, int b) {
         return (255 << 24) | (255 << 16) | (255 << 8) | 255;
     }
