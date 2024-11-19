d7c382111b8abd50b31e4317eedb5a49db8cf68a
Mark Ra <mark.ra.vilbiks@gmail.com>
2024-11-19 05:27:17 +0200

    first element implementation

--- main.java
+++ main.java
@@ -1,28 +1,34 @@
 import java.awt.image.BufferedImage;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 class Program {
+    private static final int height = 300; 
+    private static final int width = 500; 
+    private static BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    private static Entity entity;
+
     public static void main(String[] args) {
-        int height = 300; 
-        int width = 500; 
         int alpha = 255; 
         int red = 0; 
         int green = 0; 
         int blue = 0; 
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int rgb = (alpha << 24) | (red << 16) | (green << 8) | blue;
                 image.setRGB(x, y, rgb);  // Set pixel color
             }
         }
+        entity = new Entity(width/2, height/2);
 
-        int x = (int) Math.round(width*0.5);
-        int y = (int) Math.round(height*0.5);
-        image.setRGB(x, y, (255 << 24) | (255 << 16) | (255 << 8) | 255);
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(Program::run, 1, 500, TimeUnit.MILLISECONDS);
 
         JFrame frame = new JFrame("my cool window!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,5 +36,9 @@ class Program {
         frame.add(new JLabel(new ImageIcon(image)));
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static void run() {
+        image.setRGB(x, y, (255 << 24) | (255 << 16) | (255 << 8) | 255);
     }
 }
