da6024c22ed0e3467722b6e102e0de35684978ff
Mark Ra <mark.ra.vilbiks@gmail.com>
2024-11-19 05:00:21 +0200

    entity x and y

--- main.java
+++ main.java
@@ -1,16 +1,17 @@
 import java.awt.image.BufferedImage;
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 class Program {
     public static void main(String[] args) {
-        int height = 50; 
-        int width = 50; 
-        int alpha = 117; 
-        int red = 159; 
-        int green = 50; 
-        int blue = 50; 
+        int height = 300; 
+        int width = 500; 
+        int alpha = 255; 
+        int red = 0; 
+        int green = 0; 
+        int blue = 0; 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
@@ -19,9 +20,14 @@ class Program {
             }
         }
 
+        int x = (int) Math.round(width*0.5);
+        int y = (int) Math.round(height*0.5);
+        image.setRGB(x, y, (255 << 24) | (255 << 16) | (255 << 8) | 255);
+
         JFrame frame = new JFrame("my cool window!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new JTextField("first window!"));// equivalent to: `frame.add(new JTextField("first window!"));`
+        frame.add(new JLabel(new ImageIcon(image)));
         frame.pack();
         frame.setVisible(true);
     }
