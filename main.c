988f76bc1576cb01f5066bbd80dd9a33a5fe19d2
Mark Ra <mark.ra.vilbiks@gmail.com>
2025-01-15 20:51:51 +0200

    fixed underflow with decay

--- main.c
+++ main.c
@@ -21,10 +21,11 @@ int main() {
     Uint32 height = 1080;
     Uint32 entity_x = width/2;
     Uint32 entity_y = height/2;
-    Uint32 running = 1000; // how many times to run
+    Uint32 running = 500; // how many times to run
     Uint32 frame_delay = 1000/100; // FPS in milliseconds basically
     Uint8 color[4] = {200, 255, 255, 255}; // RGBA
     Uint8 clear_color[4] = {0, 0, 0, 255}; // RGBA
+    Uint8 color_decay[4] = {1, 4, 2, 0}; // RGBA
     Uint32 entity_updates_per_draw = 2000;
     Uint32 entity_update_count = 0;
 
@@ -33,6 +34,7 @@ int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_SHOWN, &window, &renderer);
     SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+    printf("2"); fflush(stdout);
     SDL_Event* event = NULL;
     Uint8 image[width*4*height]; // formated as [r,g,b,a,r,g ... a,r,g,b,a]
     void* pixels;
@@ -64,12 +66,13 @@ int main() {
         }
 
         for (Uint32 i = 0; i < width*4*height; i+=4) {
-            image[i+0] = image[i+0] <= 0 ? image[i+0] : image[i+0] - 2; // R
-            image[i+1] = image[i+1] <= 0 ? image[i+1] : image[i+1] - 4; // G
-            image[i+2] = image[i+2] <= 0 ? image[i+2] : image[i+2] - 1; // B
-            image[i+3] = image[i+3] <= 0 ? image[i+3] : image[i+3] - 0; // A
+            image[i+0] = image[i+0] <= color_decay[0] ? image[i+0] : image[i+0] - color_decay[0];
+            image[i+1] = image[i+1] <= color_decay[1] ? image[i+1] : image[i+1] - color_decay[1];
+            image[i+2] = image[i+2] <= color_decay[2] ? image[i+2] : image[i+2] - color_decay[2];
+            image[i+3] = image[i+3] <= color_decay[3] ? image[i+3] : image[i+3] - color_decay[3];
         }
         SDL_UpdateTexture(texture, NULL, image, width*4);
+        printf("count: %d -- loc x: %d, y: %d\n", running, entity_x, entity_y);
 
         entity_update_count = 0;
         if (SDL_PollEvent(event) && event != NULL && event->type == SDL_QUIT) {
